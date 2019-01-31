97648a1e8293bf864b6c6d0bb5675c3e141b1f24
Prem Rajah <prajah@ctlondon.com>
2019-01-31 14:28:59 +0000

    finished select controls validation, starting form validation

--- src/app/angular-form-tdf/angular-form-tdf.component.ts
+++ src/app/angular-form-tdf/angular-form-tdf.component.ts
@@ -9,12 +9,22 @@ import { User } from '../user';
 export class AngularFormTdfComponent implements OnInit {
 
   topics = ["Angular", "React", "Vue"];
+  topicHasError = true;
 
-  userModel = new User("", "Prem@gmail.com", 758594562, "", "morning", true);
+  userModel = new User("", "Prem@gmail.com", 758594562, "default", "morning", true);
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  /// validate the select tag 
+  validateTopic(value: any) {
+    if(value === 'default') {
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false;
+    }
   }
 
 }
